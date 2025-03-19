5bb43d689148bddc4981e7414b7c2499faae4a22
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-03-19 15:21:00 +0100

    add demo grid

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Server, status: 301 },
   {
     path: '**',
     renderMode: RenderMode.Prerender,
